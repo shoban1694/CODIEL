b49718cf46dd4aeb4fc37296e5968e4520a6bfbc
shobanraj N V <shoban.games@gmail.com>
2022-04-02 20:21:33 +0530

    creating sign up

---
+++ config/mongoose.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost/mydb");
+
+const db = mongoose.connection;
+
+db.on('error',console.error.bind('console','Unable to establish connection'));
+
+db.once('open',function(){
+     console.log("Mongo DB connected successfuly");
+})
